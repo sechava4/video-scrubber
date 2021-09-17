9da934e5ae74082b11b3bd55d4315e064e0d6113
sechava4 <sechava4@eafit.edu.co>
2021-09-17 11:07:45 -0500

    Include postman collection

--- flaskapp/config.py
+++ flaskapp/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config(object):
     SECRET_KEY = (
-        os.environ.get("SECRET_KEY") or "adljsakldjk72s4e21cjn!Ew@fhfghfghggg4565t@dsa"
+        os.environ.get("SECRET_KEY") or "adljsakldjk72s4e21cjn!Ew@fhfghfghggg456t@dsa"
     )
     SQLALCHEMY_DATABASE_URI = (
         os.environ.get("DATABASE_URL")
